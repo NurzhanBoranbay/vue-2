aca9b59b524dfb5dae84ba9af84d33d37417d0a9
NurzhanBoranbay <boranbaynurzhan3097@gmail.com>
2021-08-29 17:18:03 +0600

    commit exercise-2

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <filtered-users />
+</template>
+
+<script>
+import FilteredUsers  from './components/FilteredUsers.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FilteredUsers 
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/FilteredUsers.vue
@@ -0,0 +1,50 @@
+<template>
+   <div class="conatiner">
+      <h2>Я найду тебя!</h2>
+      <input 
+         type="text" 
+         name="text"
+         placeholder="Искать"
+         @input="searchText = $event.target.value"
+      >
+      <h5>Всего имен: {{ users.length }}, совпадений: {{ getFilteredName.length }}</h5>
+      <p
+         v-for="(user, index) in getFilteredName"
+         :key="index"
+      >
+         {{ user }}
+      </p>
+  </div>
+
+</template>
+
+<script>
+export default {
+   name: 'FilteredUsers',
+   data() {
+      return {
+         searchText: '',
+         users: ['Расул', 'Самат', 'Еркебулан', 'Сабит', 'Нуржан'],
+      }
+   },
+
+   computed: {
+      getFilteredName() {
+         return this.users.filter(user => user.toLowerCase().includes(this.searchText.toLowerCase()))
+      }
+   }
+
+   //watch: {
+   //   currentPage(page) {
+   //      this.loadUsers(page)
+   //   }
+   //}
+
+}
+</script>
+
+<style src="@/assets/styles/main.css">
+
+
+
+</style>
